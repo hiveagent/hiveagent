aabc2a2f545553ba7fe9d68c4bb1ba4d42e309c9
MCPAgentAI Dev <test4automationsdk@gmail.com>
2025-01-20 18:09:02 +0100

    release 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hiveagentai",
-    version="0.0.2",
+    version="0.1.0",
     author="HiveagentAI Dev",
     author_email="dev@hiveagentai.com",
     description="The ultimate platform for orchestrating intelligent teamwork.",
